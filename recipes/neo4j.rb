9a89555a9f06efcd6f64da6012958b80ef3d094d
Alex Willemsma <alex@undergroundwebdevelopment.com>
2014-01-23 14:11:43 -1000

    Added a template & configs for the neo4j plist so that neo4j will start on boot.

--- recipes/neo4j.rb
+++ recipes/neo4j.rb
@@ -1,8 +1,19 @@
 unless brew_installed? "neo4j"
   package "neo4j"
 
-  execute "install neo4j to start on boot" do
-    command "`brew --prefix neo4j`/bin/neo4j install"
+  directory "/Users/#{node['current_user']}/Library/LaunchAgents" do
+    owner node['current_user']
+    action :create
+  end
+
+  template "#{node['sprout']['home']}/Library/LaunchAgents/homebrew.mxcl.neo4j.plist" do
+    source "neo4j.plist.erb"
+    variables ({ :neo4j_prefix => `brew --prefix neo4j`.chomp })
+    user node['current_user']
+  end
+
+  execute "load the neo4j plist into the mac daemon startup thing" do
+    command "launchctl load -w #{node['sprout']['home']}/Library/LaunchAgents/homebrew.mxcl.neo4j.plist"
     user node['current_user']
   end
 end
